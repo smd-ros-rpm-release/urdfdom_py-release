aa244c91ef16da6532070946db8ca96e7e7c9ab5
isucan <isucan@gmail.com>
2013-09-10 15:27:07 +0300

    Rebase from 'release/hydro/urdfdom_py'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
  
 d = generate_distutils_setup(
-    packages=['urdf_parser_py'],
+    packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
     package_dir={'': 'urdf_parser_py/src'}
 )
 
